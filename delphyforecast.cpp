bc8097c0b607cbf65bbec39a7e3202c26ceb3753
OracleChain <contact@oraclechain.io>
2018-05-24 12:56:32 +0800

    ok

--- delphyforecast.cpp
+++ delphyforecast.cpp
@@ -1,13 +1,111 @@
 #include <eosiolib/eosio.hpp>
+#include "delphyforecast.hpp"
 
 using namespace eosio;
 
+void Delphyforecast::clearquery(uint64_t id){
+    require_auth(admin);
 
-
-
-extern "C" {
-    /// The apply method implements the dispatch of events to this contract
-    void apply( uint64_t receiver, uint64_t code, uint64_t action ) {
-
+    OralceQureyContainer oralceQureyContainer(_self, admin);
+    auto ite = oralceQureyContainer.find(id);
+    if(ite != oralceQureyContainer.end()){
+        oralceQureyContainer.erase(ite);
     }
+}
+
+void Delphyforecast::addqurey( OracleQuery oq ) {
+    OralceQureyContainer oralceQureyContainer(_self, admin);
+    auto ite = oralceQureyContainer.find(oq.id);
+
+    auto iteQueryId = oralceQureyContainer.rbegin();
+    uint64_t id = 0;
+    if(iteQueryId == oralceQureyContainer.rend()){
+        id = 0;
+    }else{
+        id = iteQueryId->id + 1;
+    }
+
+    oralceQureyContainer.emplace(oq.from, [&](auto &s){
+        s.id            = id;
+        s.from          = oq.from;
+        s.roles         = oq.roles;
+        s.mode          = oq.mode;
+        s.mode_detail   = oq.mode_detail;
+        s.type          = oq.type;
+        s.aspect        = oq.aspect;
+        s.provider      = oq.provider;
+        s.startdate     = oq.startdate;
+        s.enddate       = oq.enddate;
+        s.conditions    = oq.conditions;
+        s.appealenddate  = oq.appealenddate;
+        s.unit           = oq.unit;
+        s.status         = QUERY_STATUS_NOT_DEAL;
+        s.created        = now();
+        s.updated        = now();
+    });
+}
+
+void Delphyforecast::addappeal(const Appeal & a){
+    OralceQureyContainer oralceQureyContainer(_self, admin);
+    auto iteQuery = oralceQureyContainer.find(a.idquery);
+    eosio_assert(iteQuery != oralceQureyContainer.end(), QUERY_EXPIRED);
+
+    AppealContainer appealContainer(_self, admin);
+    auto ite = appealContainer.rbegin();
+    uint64_t id = 0;
+    if(ite == appealContainer.rend()){
+        id = 0;
+    }else{
+        id = ite->id + 1;
+    }
+
+    appealContainer.emplace(admin, [&](auto &s){
+          s.id      = id     ;
+          s.idquery = a.idquery;
+          s.from    = a.from   ;
+          s.reason  = a.reason ;
+          s.created = now();
+          s.updated = now();
+    });
+}
+
+
+void Delphyforecast::addanswer(const QueryResult & queryResult){
+
+    OralceQureyContainer oralceQureyContainer(_self, admin);
+    auto iteQuery = oralceQureyContainer.find(queryResult.idquery);
+    eosio_assert(iteQuery!= oralceQureyContainer.end(), QUERY_NOT_EXISTS);
+    eosio_assert(iteQuery->enddate>now(), TIME_NOT_REACHED);
+
+    QueryResultContainer queryResultContainer(_self, admin);
+    auto ite = queryResultContainer.find(queryResult.idquery);
+
+    if(ite != queryResultContainer.end()){
+        queryResultContainer.emplace(admin, [&](auto&s){
+            s.idquery  = queryResult.idquery;
+            s.checked  = queryResult.checked;
+            s.appealnum  = queryResult.appealnum;
+            s.readvalue  = queryResult.readvalue;
+            s.result  = queryResult.result;
+            s.unitrate  = queryResult.unitrate;
+            s.created  = queryResult.created;
+            s.updated  = queryResult.updated;
+        });
+    }else{
+          queryResultContainer.modify(queryResult, admin, [&](auto&s){
+              s.idquery  = queryResult.idquery;
+              s.checked  = queryResult.checked;
+              s.appealnum  = queryResult.appealnum;
+              s.readvalue  = queryResult.readvalue;
+              s.result  = queryResult.result;
+              s.unitrate  = queryResult.unitrate;
+              s.created  = queryResult.created;
+              s.updated  = queryResult.updated;
+          });
+    }
+
+    oralceQureyContainer.modify(iteQuery, admin, [&](auto &s){
+       s.status = QUERY_STATUS_DEALED;
+    });
+    eosio::print("version12");
 }
--- delphyforecast.hpp
+++ delphyforecast.hpp
@@ -9,15 +9,19 @@
 #define QUERY_ALREADY_EXISTS "6000001 QUERY_ALREADY_EXISTS" //参数错误
 #define QUERY_NOT_EXISTS "6000002 QUERY_NOT_EXISTS"
 #define QUERY_EXPIRED   "6000003 QUERY_EXPIRED"
+#define TIME_NOT_REACHED "6000004 TIME_NOT_REACHED"
 
+static long QUERY_STATUS_NOT_DEAL = 0l;
+static long QUERY_STATUS_DEALED = 1l;
+
+using namespace eosio;
 /*
-@abi action
 @abi table
 */
-struct OralceQurey{
+struct OracleQuery{
     uint64_t id;
     account_name from;
-    std::vector<std::string> roles;//["BTC/USD"],
+    std::string roles;//["BTC/USD"],
 
     std::string mode;//"normal", // 表示原值
     std::string mode_detail;//ration|value等取值
@@ -29,7 +33,7 @@ struct OralceQurey{
     uint64_t startdate;//15234345", //取结果的时间,utc 单位秒
     uint64_t enddate;//15234345", //取结果的时间,utc 单位秒
 
-    std::vector<std::string> conditions;//:[">=10000","<10000"],//大于等于 1 万，小于 1 万
+    std::string conditions;//:[">=10000","<10000"],//大于等于 1 万，小于 1 万
     uint64_t    appealenddate;//"43200" // 申诉时间,问题发起方，可能对此问题的答案有异议
     std::string unit;//usd或者cny等单位
 
@@ -40,21 +44,20 @@ struct OralceQurey{
 
     uint64_t primary_key()const { return id; }
 
-    EOSLIB_SERIALIZE(OralceQurey, (id)(from)(roles)(mode)(mode_detail)(type)(aspect)(provider)(startdate)(enddate)(conditions)(appealenddate)(unit)(status)(created)(updated));
+    EOSLIB_SERIALIZE(OracleQuery, (id)(from)(roles)(mode)(mode_detail)(type)(aspect)(provider)(startdate)(enddate)(conditions)(appealenddate)(unit)(status)(created)(updated))
 };
 
-typedef eosio::multi_index<N(OralceQurey), OralceQurey> OralceQureyContainer;
+
 
 /*
-@abi action
-@abi table
+@abi table appeal i64
 */
 struct Appeal{
     uint64_t id;
     uint64_t idquery;
 
     account_name from;
-    uint64_t reason;
+    std::string reason;
 
     uint64_t created;
     uint64_t updated;
@@ -62,13 +65,14 @@ struct Appeal{
     uint64_t primary_key()const { return id; }
     uint64_t get_secondary()const { return idquery; }
 
-    EOSLIB_SERIALIZE(Appeal, (id)(from)(idquery)(reason)(created)(updated));
+    EOSLIB_SERIALIZE(Appeal, (id)(idquery)(from)(reason)(created)(updated))
 };
 
-typedef eosio::multi_index<N(Appeal), Appeal,
-   eosio::indexed_by< N(bysecondary), eosio::const_mem_fun<Appeal,  uint64_t, &Appeal::get_secondary> >> AppealContainer;
 
 
+/*
+@abi table
+*/
 struct QueryResult{
     uint64_t idquery;
 
@@ -83,73 +87,37 @@ struct QueryResult{
 
     uint64_t primary_key()const { return idquery; }
 
-    EOSLIB_SERIALIZE(QueryResult, (idquery)(checked)(appealnum)(readvalue)(result)(unitrate)(created)(updated));
+    EOSLIB_SERIALIZE(QueryResult, (idquery)(checked)(appealnum)(readvalue)(result)(unitrate)(created)(updated))
 };
 
-typedef eosio::multi_index<N(QueryResult), QueryResult> QueryResultContainer;
+typedef eosio::multi_index<N(oraclequery), OracleQuery> OralceQureyContainer;
+typedef eosio::multi_index<N(queryresult), QueryResult> QueryResultContainer;
+typedef eosio::multi_index<N(appeal), Appeal,
+   eosio::indexed_by< N(bysecondary), eosio::const_mem_fun<Appeal,  uint64_t, &Appeal::get_secondary> >> AppealContainer;
 
-class Delphyforecast : public eosio::contract {
-    static const uint64_t contractCode = N(delphyfore);
+class Delphyforecast : public contract {
+
     static const uint64_t admin = N(delforeadm);
 
-
     public:
-      using contract::contract;
+    Delphyforecast( account_name self )
+    :contract(self)
+    {}
 
-      /// @abi action
-      void addOralceQurey( OralceQurey oq ) {
-            OralceQureyContainer oralceQureyContainer(contractCode， admin);
-            const_iterator ite = oralceQureyContainer.find(oq.id);
-            eosio_assert(ite != oralceQureyContainer.end(), QUERY_ALREADY_EXISTS);
+    /// @abi action
+    void clearquery(uint64_t id);
 
-            oralceQureyContainer.emplace(admin, [&](auto &s){
-                s..id            = oq.id;
-                s..from          = oq.from;
-                s..roles         = oq.roles;
-                s..mode          = oq.mode;
-                s..mode_detail   = oq.mode_detail;
-                s..type          = oq.type;
-                s..aspect        = oq.aspect;
-                s..provider      = oq.provider;
-                s..startdate     = oq.startdate;
-                s..enddate;       = oq.enddate;
-                s..conditions;    = oq.conditions;
-                s..appealenddate  = oq.appealenddate;
-                s..unit;          = oq.unit;
-                s..status;        = oq.status;
-                s..created;       = oq.created;
-                s..updated;       = oq.updated;
-            });
-      }
+    /// @abi action
+    void addqurey( OracleQuery oq );
 
-      void addAppeal(const Appeal & a){
-          OralceQureyContainer oralceQureyContainer(contractCode， admin);
-          const_iterator ite = oralceQureyContainer.find(a.idquery);
-          eosio_assert(ite != oralceQureyContainer.end(), QUERY_EXPIRED);
+    /// @abi action
+    void addappeal(const Appeal & a);
 
-          AppealContainer appealContainer(contractCode, admin);
-          const_reverse_iterator ite = appealContainer.rbegin();
-          if(ite == appealContainer.rend()){
-              a.id = 0;
-          }else{
-              a.id = ite->id + 1;
-          }
-
-          appealContainer.emplace(admin, [&](auto &s){
-                s.id      = a.id     ;
-                s.idquery = a.idquery;
-                s.from    = a.from   ;
-                s.reason  = a.reason ;
-                s.created = a.created;
-                s.updated = a.updated;
-          });
-
-          auto secondary_index = appealContainer.template get_index<N(bysecondary)>();
-          secondary_index.
-      }
+    /// @abi action
+    void addanswer(const QueryResult & queryResult);
 };
 
-EOSIO_ABI( Delphyforecast, (addOralceQurey) )
+EOSIO_ABI( Delphyforecast, (addqurey)(addappeal)(addanswer)(clearquery))
 
 
 
